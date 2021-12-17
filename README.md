53a98701fb1ef4889eff95f3ac2983efabc7a22d
Philipp Schaffrath <philipp.schaffrath@gmail.com>
2021-12-18 00:39:50 +0100

    render png sprites rather than relying on figma
    reworked most of the code to be more stable, switched to rendering pngs directly in js, download exports only if the directory is supplied as input, save svg assets only if the directory is supplied as input, used node14+ features to improve code readability, structured code into functions, improved async code

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Figma Cursor Theme Generator
-GitHub Action that generates a cursor theme and exports additional assets from a Figma file.
+
+GitHub Action that generates a cursor theme from a Figma file. It can also generates cursor theme variants and cursor aliases, download SVG assets for all cursor sprites and download exports.
 
 ## Inputs
 
@@ -7,20 +8,19 @@ GitHub Action that generates a cursor theme and exports additional assets from a
 | :- | :-: | :- |
 | `figma_access_token` | string | **required** - Your personal [Figma access token](https://www.figma.com/developers/api#access-tokens). |
 | `figma_file_key` | string | **required** - The `key` of the Figma file containing the cursor theme. Can be found in the url to of a figma file. `https://www.figma.com/file/:key/:title` |
-| `sprite_component_set_id` | string | **required** - Figma node ID of the sprite component set. Refer to the [Component Set ID](#component-set-id) section to find this ID. |
-| `alias_component_set_id` | string | **required** - Figma node ID of the alias component set. Refer to the [Component Set ID](#component-set-id) section to find this ID. |
+| `sprite_component_set_id` | string | **required** - Figma node ID of the sprite component set. Refer to the [Figma File](#figma-file) section to find this ID. |
+| `alias_component_set_id` | string | **required** - Figma node ID of the alias component set. Refer to the [Figma File](#figma-file) section to find this ID. |
 | `theme_name` | string | **required** - Name of the cursor theme. Default `"Generated Cursor Theme"`. |
 | `theme_comment` | string | **required** - Short comment or description of the cursor theme. Default `"This is a generated cursor theme."`. |
-| `output_directory` | string | The directory to download assets and generate the cursor theme into. Default `"build"`. |
+| `theme_directory` | string | **required** - Output directory for the cursor theme variants. Default `build/themes`. |
+| `export_directory` | string | Output directory for exports defined in the Figma file. Figma exports will only be downloaded if this is defined. |
+| `svg_directory` | string | Output directory for SVG images of all cursor sprites. SVG assets of sprites will only be generated if this is defined. |
 
 ## Outputs
+
 | Name | Type | Description |
 | :- | :-: | :- |
 | `version` | integer | The current version of the Figma file. |
-| `theme_directory` | string | Directory containing the generated theme variants. |
-| `svg_directory` | string | Directory containing all sprites rendered as svg files. |
-| `export_directory` | string | Directory containing exports defined in the Figma file. |
-
 
 ## Example
 
@@ -28,26 +28,33 @@ GitHub Action that generates a cursor theme and exports additional assets from a
 steps:
   - name: Generate cursor theme from Figma file
     id: figma_cursor_theme
-    uses: phisch/figma-cursor-theme-action@v0.6.0
+    uses: phisch/figma-cursor-theme-action@v1.0
     with:
       figma_access_token: ${{ secrets.FIGMA_ACCESS_TOKEN }}
       figma_file_key: ${{ secrets.FIGMA_FILE_KEY }}
       alias_component_set_id: '13:37'
       sprite_component_set_id: '73:31'
-      output_directory: 'build'
       theme_name: 'Super Cool Cursors'
       theme_comment: 'Very awesome and cool cursor theme.'
+      theme_directory: 'build/themes'
+      export_directory: 'build/exports'
+      svg_directory: 'build/svg'
 ```
 
-## Figma file
+## Figma File
+
 This generator requires a Figma file with specific Figma components. The IDs of those components have to be defined as inputs for this GitHub action. More information under [Component Set ID](#component-set-id).
+
+> To aquire the component set ID, click the component and take it from the `node-id` parameter in the URL `https://www.figma.com/file/:key/template?node-id=:node-id`. This parameter is URL encoded, so make sure to replace `%3A` with `:`. A valid component set ID looks like this: `13:37`
 
 The components can be created manually, or you can use the very basic [template.fig](template.fig), which comes with those two components already created.
 
 ### Sprite Component
+
 Variants of the sprite component are used to generate cursor files.
 
 #### Properties
+
 | Name | Type | Description |
 | :- | :-: | :- |
 | `cursor` | string | Name of the cursor this sprite belongs to. All sprites with the same cursor name end up in the same cursor file. |
@@ -59,18 +66,18 @@ Variants of the sprite component are used to generate cursor files.
 | `variant` | string | The cursor theme variant this sprite belongs to. Setting variant to anything other than `default` will generate a secondary cursor theme with this cursor. This variant will automatically inherit all cursors from the default variant. |
 
 ### Alias Component
+
 The alias component is used to create cursor symlinks. This is helpful to link legacy cursor names to the ones provided through the sprites, or to use the same sprite for multiple cursors.
 
 #### Properties
+
 | Name | Type | Description |
 | :- | :-: | :- |
 | `alias` | string | Name of the alias. This will be the file name of the symlink. |
 | `name` | string | Name of the cursor this is an alias for. This will be the symlink target. |
 
-### Exports
-Any export defined in the figma file will be exported to the `export_directory`. This can be used to get up to date previews of the cursor theme.
+## Exports and SVG Sprites
 
-In addition to that, a SVG version of every sprite is generated into the `svg_directory`.
+If the input `export_directory` is defined, this action will download every export that is defined in the Figma file to that directory. This can be used to automatically generate preview images.
 
-### Component Set ID
-To aquire the component set ID, click the component and take it from the `node-id` parameter in the URL `https://www.figma.com/file/:key/template?node-id=:node-id`. This parameter is URL encoded, so make sure to replace `%3A` with `:`. A valid component set ID looks like: `13:37`
+Additionally, if the `svg_directory` input is defined, a SVG version of every cursor sprite will be saved in that directory.
--- action.js
+++ action.js
@@ -1,242 +1,210 @@
 const core = require('@actions/core');
-const fs = require('fs');
+const fs = require('fs-extra');
 const https = require('https');
 const path = require('path');
 const Figma = require('figma-js');
-const pixels = require('image-pixels');
 const { Encoder } = require('xcursor');
+const sharp = require('sharp');
+const got = require('got');
 
 (async () => {
   try {
     const FIGMA_ACCESS_TOKEN = core.getInput('figma_access_token', { required: true });
     const FIGMA_FILE_KEY = core.getInput('figma_file_key', { required: true });
-    const OUTPUT_DIRECTORY = path.normalize(core.getInput('output_directory', { required: true }));
+
+    const ALIAS_COMPONENT_SET_ID = core.getInput('alias_component_set_id', { required: true });
+    const SPRITE_COMPOENT_SET_ID = core.getInput('sprite_component_set_id', { required: true });
+
+    const THEME_DIRECTORY = core.getInput('theme_directory', { required: true });
+    const EXPORT_DIRECTORY = core.getInput('export_directory');
+    const SVG_DIRECTORY = core.getInput('svg_directory');
+
     const THEME_NAME = core.getInput('theme_name', { required: true });
     const THEME_COMMENT = core.getInput('theme_comment');
 
-    const client = Figma.Client({
-      personalAccessToken: FIGMA_ACCESS_TOKEN,
-    });
+    const client = Figma.Client({ personalAccessToken: FIGMA_ACCESS_TOKEN });
 
     const file = await client.file(FIGMA_FILE_KEY);
 
     if (file.status !== 200) {
-      throw Error(`Failed to fetch file with key '${FIGMA_FILE_KEY}'.`);
+      throw Error(`Failed to fetch Figma file '${FIGMA_FILE_KEY}' with status code ${file.status}`);
     }
 
     core.setOutput('version', file.data.version);
 
-    const download = async (url, filePath) => {
-      const dir = path.dirname(filePath);
-      if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir, { recursive: true });
-      }
-      const request = https.get(url, (response) => {
-        if (response.statusCode !== 200) {
-          throw Error(`Failed to download ${url} with status code ${response.statusCode}.`);
-        }
-        response.pipe(fs.createWriteStream(filePath));
+    const getExports = (node, previousExports = {}) => {
+      const exports = previousExports;
+      node.exportSettings?.forEach((settings) => {
+        const format = settings.format.toLowerCase();
+        const scale = settings.constraint.value;
+        exports[format] ??= {};
+        exports[format][scale] ??= {};
+        exports[format][scale][node.id] = `${node.name}${settings.suffix ? `_${settings.suffix}` : ''}.${format}`;
       });
-      await new Promise((resolve, reject) => {
-        request.on('error', reject);
-        request.on('finish', resolve);
-      });
-      core.debug(`downloaded ${filePath} from ${url}`);
+      node.children?.forEach((child) => getExports(child, exports));
+      return exports;
     };
 
-    const exports = {};
-    const collectExportsRecursively = (node) => {
-      if (node.exportSettings) {
-        node.exportSettings.forEach((settings) => {
-          const format = settings.format.toLowerCase();
-          const scale = settings.constraint.value;
-          const fileName = `${node.name}${settings.suffix ? `_${settings.suffix}` : ''}.${format}`;
-          exports[format] = exports[format] || {};
-          exports[format][scale] = exports[format][scale] || {};
-          exports[format][scale][node.id] = fileName;
-        });
+    const downloadFigmaExports = async (exports) => {
+      if (!fs.existsSync(EXPORT_DIRECTORY)) {
+        fs.mkdirSync(EXPORT_DIRECTORY, { recursive: true });
       }
-      if (node.children) {
-        node.children.forEach(collectExportsRecursively);
-      }
-    };
-    collectExportsRecursively(file.data.document);
-
-    const exportDirectory = path.join(OUTPUT_DIRECTORY, 'export');
-    core.setOutput('export_directory', exportDirectory);
-
-    Object.entries(exports).forEach(async ([format, scales]) => {
-      Object.entries(scales).forEach(async ([scale, nodes]) => {
-        await client.fileImages(FIGMA_FILE_KEY, {
-          ids: Object.keys(nodes),
-          scale,
-          format,
-        }).then(async (imagesResponse) => {
-          core.info(`Downloading ${Object.keys(imagesResponse.data.images).length} sprite ${format} files for ${scale}x scale.`);
-          const downloads = [];
-          Object.entries(imagesResponse.data.images).forEach(([id, url]) => {
-            const filePath = path.join(exportDirectory, exports[format][scale][id]);
-            downloads.push(download(url, filePath));
+      await Promise.all(Object.entries(exports).map(async ([format, scales]) => {
+        await Promise.all(Object.entries(scales).map(async ([scale, nodes]) => {
+          const imagesResponse = await client.fileImages(FIGMA_FILE_KEY, {
+            ids: Object.keys(nodes),
+            scale,
+            format,
           });
-          await Promise.all(downloads);
-        }).catch((error) => {
-          core.setFailed(error.message);
-        });
-      });
-    });
+          if (imagesResponse.status !== 200) {
+            throw Error(`Failed to fetch Figma file images with status code ${imagesResponse.status}.`);
+          }
+          await Promise.all(Object.entries(imagesResponse.data.images).map(async ([id, url]) => {
+            const filePath = path.join(EXPORT_DIRECTORY, exports[format][scale][id]);
+            const request = https.get(url, (response) => {
+              if (response.statusCode !== 200) {
+                throw Error(`Failed to download ${url} with status code ${response.statusCode}.`);
+              }
+              response.pipe(fs.createWriteStream(filePath));
+            });
+            await new Promise((resolve, reject) => {
+              request.on('error', reject);
+              request.on('finish', resolve);
+            });
+            core.debug(`downloaded ${filePath} from ${url}`);
+          }));
+        }));
+      }));
+    };
 
-    const getComponentsFromSetId = (setId) => Object.entries(file.data.components).reduce(
-      (acc, [key, value]) => (value.componentSetId === setId ? { ...acc, [key]: value } : acc),
-      {},
-    );
+    if (EXPORT_DIRECTORY) {
+      await downloadFigmaExports(getExports(file.data.document));
+    }
 
-    const parseProperties = (propertiesString) => propertiesString.split(', ')
-      .reduce((acc, cur) => {
-        const [key, value] = cur.split('=');
-        return { ...acc, [key]: value };
-      }, {});
+    const parseProperties = (propertiesString) => propertiesString.split(', ').reduce((acc, cur) => {
+      const [key, value] = cur.split('=');
+      return { ...acc, [key]: value };
+    }, {});
 
-    const ALIAS_COMPONENT_SET_ID = core.getInput('alias_component_set_id', { required: true });
-    const aliases = Object.values(getComponentsFromSetId(ALIAS_COMPONENT_SET_ID))
-      .map((aliasComponent) => parseProperties(aliasComponent.name));
-    core.debug(`Found ${aliases.length} defines aliases in the Figma file.`);
+    const aliases = Object.entries(file.data.components)
+      .filter(([, value]) => value.componentSetId === ALIAS_COMPONENT_SET_ID)
+      .map(([, value]) => parseProperties(value.name));
 
-    const SPRITE_COMPOENT_SET_ID = core.getInput('sprite_component_set_id', { required: true });
-    const spriteComponents = getComponentsFromSetId(SPRITE_COMPOENT_SET_ID);
-    core.debug(`Found ${Object.keys(spriteComponents).length} defined sprites in the Figma file.`);
+    const getSprites = async (node) => {
+      const sprites = Object.entries(node)
+        .filter(([, value]) => value.componentSetId === SPRITE_COMPOENT_SET_ID)
+        .reduce((acc, [key, value]) => ({
+          ...acc,
+          [key]: { ...parseProperties(value.name), images: {} },
+        }), {});
 
-    const sprites = {};
-
-    Object.entries(spriteComponents).forEach(([id, spriteComponent]) => {
-      sprites[id] = { ...parseProperties(spriteComponent.name), images: {} };
-    });
-
-    await Promise.all([1, 2, 4].map(async (scale) => {
-      core.info(`Requesting Figma export for sprites at scale ${scale}x.`);
       const response = await client.fileImages(FIGMA_FILE_KEY, {
-        ids: Object.keys(spriteComponents),
-        scale,
-        format: 'png',
+        ids: Object.keys(sprites),
+        scale: 1,
+        format: 'svg',
       });
       if (response.status !== 200) {
         throw Error(`Failed to fetch file images with status code ${response.status}.`);
       }
-      core.info(`Received ${Object.keys(response.data.images).length} image urls for scale ${scale}x, downloading them now.`);
-      const imageFiles = await pixels.all(response.data.images);
-      Object.entries(imageFiles).forEach(([id, image]) => {
-        if (image.width !== image.height) {
-          throw Error(`Image with id '${id}' is not square at scale ${scale}.`);
-        }
-        const { data } = image;
-        for (let i = 0; i < data.length; i += 4) {
-          const temp = image.data[i];
-          data[i] = image.data[i + 2];
-          data[i + 2] = temp;
-        }
-        sprites[id].images[image.width] = {
-          scale,
-          data,
-        };
-      });
-      Promise.resolve();
-    }));
 
-    const svgDirectory = path.join(OUTPUT_DIRECTORY, 'svg');
-    core.setOutput('svg_directory', svgDirectory);
-    await client.fileImages(FIGMA_FILE_KEY, {
-      ids: Object.keys(spriteComponents),
-      scale: 1,
-      format: 'svg',
-    }).then(async (imagesResponse) => {
-      core.info(`Downloading ${Object.keys(imagesResponse.data.images).length} sprite svg files.`);
-      const downloads = [];
-      Object.entries(imagesResponse.data.images).forEach(([id, url]) => {
-        const filePath = path.join(svgDirectory, sprites[id].base, sprites[id].variant, `${sprites[id].cursor}.svg`);
-        downloads.push(download(url, filePath));
-      });
-      await Promise.all(downloads);
-    }).catch((error) => {
-      core.setFailed(error.message);
-    });
+      await Promise.all(Object.entries(response.data.images).map(async ([id, url]) => {
+        core.debug(`Downloading sprite ${id} from ${url}`);
+        const buffer = await got(url).buffer();
 
-    const variants = {};
-    Object.entries(sprites).forEach(([id, sprite]) => {
-      variants[sprite.variant] = variants[sprite.variant] || {};
-      variants[sprite.variant][sprite.cursor] = variants[sprite.variant][sprite.cursor] || {};
+        if (buffer.length === 0) {
+          core.warning(`Skipping empty sprite with id ${id} and url ${url}.`);
+          return;
+        }
+
+        const meta = await sharp(buffer).metadata();
+        if (meta.width !== meta.height) {
+          throw Error(`Sprite with id '${id}' is not a square.`);
+        }
+
+        if (SVG_DIRECTORY) {
+          const svgFilePath = path.join(
+            SVG_DIRECTORY,
+            sprites[id].base,
+            sprites[id].variant,
+            `${sprites[id].cursor}${sprites[id].frame > 0 ? `_${sprites[id].frame}` : ''}.svg`,
+          );
+          fs.ensureDirSync(path.dirname(svgFilePath));
+          fs.writeFileSync(svgFilePath, buffer);
+        }
+
+        await Promise.all([1, 2, 4].map(async (scale) => {
+          const rawBuffer = await sharp(buffer, { density: 72 * scale })
+            .resize(meta.width * scale, meta.height * scale)
+            .raw()
+            .toBuffer();
+          for (let i = 0; i < rawBuffer.length; i += 4) {
+            const red = rawBuffer[i];
+            rawBuffer[i] = rawBuffer[i + 2];
+            rawBuffer[i + 2] = red;
+          }
+          sprites[id].images[meta.width * scale] = { scale, buffer: rawBuffer };
+        }));
+      }));
+
+      return sprites;
+    };
+
+    const sprites = await getSprites(file.data.components);
+    const variants = Object.entries(sprites).reduce((acc, [id, sprite]) => {
+      acc[sprite.variant] ??= {};
+      acc[sprite.variant][sprite.cursor] ??= {};
       Object.entries(sprite.images).forEach(([size]) => {
-        variants[sprite.variant][sprite.cursor][size] = (
-          variants[sprite.variant][sprite.cursor][size] || {}
-        );
-        variants[sprite.variant][sprite.cursor][size][sprite.frame] = { spriteId: id };
+        acc[sprite.variant][sprite.cursor][size] ??= {};
+        acc[sprite.variant][sprite.cursor][size][sprite.frame] = id;
       });
-    });
+      return acc;
+    }, {});
 
-    const slugify = (str) => str.toLowerCase().replace(/\s+/g, '-').replace(/[^a-z0-9-]/g, '');
-
-    const themeDirectory = path.join(OUTPUT_DIRECTORY, 'themes');
-    core.setOutput('theme_directory', themeDirectory);
+    const getSlug = (str) => str.toLowerCase().replace(/\s+/g, '-').replace(/[^a-z0-9-]/g, '');
 
     Object.entries(variants).forEach(([variant, cursors]) => {
-      const variantDirectory = path.join(
-        themeDirectory,
-        slugify(variant === 'default' ? THEME_NAME : `${THEME_NAME} ${variant}`),
-      );
-
-      fs.mkdirSync(path.join(variantDirectory, 'cursors'), { recursive: true });
-
+      const slug = getSlug(`${THEME_NAME}${variant === 'default' ? '' : ` ${variant}`}`);
+      const variantDirectory = path.join(THEME_DIRECTORY, slug);
+      const cursorsDirectory = path.join(variantDirectory, 'cursors');
+      fs.ensureDirSync(cursorsDirectory);
       Object.entries(cursors).forEach(([cursor, sizes]) => {
         core.info(`Generating cursor '${cursor}' for variant '${variant}'.`);
-        const images = [];
-        Object.entries(sizes).forEach(([size, frames]) => {
+        const encoder = new Encoder(Object.entries(sizes).flatMap(([size, frames]) => {
           const animated = Object.keys(frames).length > 1;
-          Object.values(frames).forEach((frame) => {
-            images.push({
-              type: size,
-              xhot: sprites[frame.spriteId].xhot * sprites[frame.spriteId].images[size].scale,
-              yhot: sprites[frame.spriteId].yhot * sprites[frame.spriteId].images[size].scale,
-              data: sprites[frame.spriteId].images[size].data,
-              delay: animated ? sprites[frame.spriteId].delay : 50,
-            });
-          });
-        });
-        const encoder = new Encoder(images);
-        fs.writeFileSync(
-          path.join(variantDirectory, 'cursors', cursor),
-          Buffer.from(encoder.pack()),
-        );
-        core.debug(`Saved cursor '${cursor}' for variant '${variant}' in '${path.join(variantDirectory, 'cursors', cursor)}'.`);
+          return Object.values(frames).map((id) => ({
+            type: size,
+            xhot: sprites[id].xhot * sprites[id].images[size].scale,
+            yhot: sprites[id].yhot * sprites[id].images[size].scale,
+            data: sprites[id].images[size].buffer,
+            delay: animated ? sprites[id].delay : 50,
+          }));
+        }));
+        fs.writeFileSync(path.join(cursorsDirectory, cursor), Buffer.from(encoder.pack()));
       });
 
       fs.writeFileSync(
         path.join(variantDirectory, 'index.theme'),
-        `[Icon Theme]\nName=${THEME_NAME}${variant === 'default' ? '' : ` - ${variant}`}\n${THEME_COMMENT ? `Comment=${THEME_COMMENT}\n` : ''}`,
+        `[Icon Theme]\nName=${THEME_NAME}${variant === 'default' ? '' : ` (${variant})`}\n${THEME_COMMENT ? `Comment=${THEME_COMMENT}\n` : ''}`,
       );
 
-      core.info(`Generated index.theme for '${variant}' variant in '${variantDirectory}'.`);
-
       if (variant !== 'default') {
-        core.info(`Symlinking default cursors that are missing in '${variant}' variant.`);
         Object.keys(variants.default).forEach((cursor) => {
           if (!variants[variant][cursor]) {
-            core.debug(`Symlinking cursor '${cursor}' from 'default' variant to '${variant}' variant.`);
+            core.info(`Symlinking cursor '${cursor}' from 'default' to '${variant}'.`);
             fs.symlinkSync(
-              path.join('../..', slugify(THEME_NAME), 'cursors', cursor),
-              path.join(variantDirectory, 'cursors', cursor),
+              path.join('../..', getSlug(THEME_NAME), 'cursors', cursor),
+              path.join(cursorsDirectory, cursor),
             );
           }
         });
       }
 
-      core.info(`Generating aliases for '${variant}' variant.`);
       aliases.forEach((alias) => {
-        core.debug(`Aliasing '${alias.cursor}' to '${alias.name}' for variant '${variant}'.`);
-        if (fs.existsSync(path.join(variantDirectory, 'cursors', alias.cursor))) {
-          fs.symlinkSync(
-            path.join(alias.cursor),
-            path.join(variantDirectory, 'cursors', alias.alias),
-          );
+        core.debug(`Creating alias '${alias.alias}' -> '${alias.cursor}' for '${variant}' variant.`);
+        if (fs.existsSync(path.join(cursorsDirectory, alias.cursor))) {
+          fs.symlinkSync(path.join(alias.cursor), path.join(cursorsDirectory, alias.alias));
         } else {
-          core.warning(`Alias '${alias.alias}' defined for cursor '${alias.cursor}' which doesn't exist.`);
+          core.warning(`Alias '${alias.alias}' is defined for cursor '${alias.cursor}' which doesn't exist.`);
         }
       });
     });
